38082f9e1b0daaaf323cf5d81309a3acb5f71a5b
Torey Heinz <th@th.local>
2011-08-06 12:41:37 -0400

    added active support, and refactored the get request

--- lib/vtiger-ruby.rb
+++ lib/vtiger-ruby.rb
@@ -1,35 +1,42 @@
 require "vt/version"
 # require 'vt/attribute_accessors'
 require 'HTTParty'
+require 'active_support/time'
+require 'active_support/core_ext/numeric'
+require 'active_support/core_ext/class/attribute_accessors'
 
 
 class VT
   cattr_accessor :user, :key, :uri
   include HTTParty
-  default_params :output => 'json'
   format :json
+
   
   def self.list_types
-    result(get(uri, :query => {:operation => 'listtypes', :sessionName => session}))
+    api_get('listtypes')
   end
       
   protected
   def self.session
-    unless @session && @challenge['expireTime'] < 30.seconds.from_now.to_i
-      @challenge  = result(get(uri, :query => {:operation => 'getchallenge', :username => user}))
-      digest     = "#{@challenge['token']}#{key}"
-      access_key = Digest::MD5.hexdigest(digest)
-      @session   = result(VT.post(uri, {:body => {:operation => 'login', :username => user, :accessKey => access_key}}))['sessionName']\
+    unless @session && @challenge['expireTime'] > 30.seconds.from_now.to_i
+      @challenge  = get(uri, :query => {:operation => 'getchallenge', :username => user})
+      digest      = "#{@challenge['token']}#{key}"
+      access_key  = Digest::MD5.hexdigest(digest)
+      @session    = post(uri, {:body => {:operation => 'login', :username => user, :accessKey => access_key}})['sessionName']
     end
     @session
   end
   
-  private
-  def self.result(query)
-    if query['success']
-      query.parsed_response['result']
-    else
-      raise "query failed"
-    end
+  def self.get(*args)
+    puts *args.inspect
+    super(*args).parsed_response['result']
+  end
+  
+  def self.api_get(operation)
+    get(uri, :query => {:operation => operation, :sessionName => session })
+  end
+  
+  def self.post(*args)
+    super(*args).parsed_response['result']
   end
 end
