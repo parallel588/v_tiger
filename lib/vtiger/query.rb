c6bae0da8bb9ee219a8ae28de48bb74b3a5e8805
Torey Heinz <ToreyHeinz@gmail.com>
2011-08-26 16:06:30 -0400

    We can query for more than a hundred products now

--- lib/vtiger/query.rb
+++ lib/vtiger/query.rb
@@ -1,5 +1,30 @@
 module Vtiger::Query
-  def query(objectType, *args)
-    get('query', :query => "select * from #{objectType}")
+  def query(objectType, options = {})
+    @objectType     = objectType
+    @options        = options
+    @query           = get('query', :query => query_string)
+    @query['result'] += get_more while more_results_needed?
+    @query
+  end
+  
+  def query_string(options = {})
+    limit = options[:limit] || @options[:limit]
+    [].tap do |query|
+      query << 'select'
+      query << '*'
+      query << "from #{@objectType}"
+      query << "limit #{limit}" if limit
+      query << ';'
+    end.join(' ')
+  end
+  
+  def more_results_needed?
+    @options[:limit].present? && @query['result'].count < @options[:limit]
+  end
+  
+  def get_more
+    offset = @query['result'].count
+    new_limit = @options[:limit] - offset
+    get('query', :query => query_string(:limit => "#{offset}, #{new_limit}"))['result']
   end
 end
--- spec/vtiger_query_spec.rb
+++ spec/vtiger_query_spec.rb
@@ -6,9 +6,27 @@ describe "Vtiger::Query" do
     @api = Vtiger.new
   end
   describe "#query" do
-    it "should return somthing" do
-      @api.query('Products', :fields => ['productname', 'manufacturer'], :where => ['discontinued is false'])['result'].should be
+    context "with just the objectType " do
+      before(:all) do
+        @query = @api.query('Products')
+      end
+      it "should return somthing" do
+        @query['result'].should be
+      end
+      
+      it "should default to 100 objects" do
+        @query['result'].count.should equal(100)
+      end
     end
     
+    context "with a limit" do
+      it "should not return more than the limit" do
+        @api.query('Products', :limit => 50)['result'].count.should_not > 50
+      end
+      
+      it "should return more than 100 is more than default" do
+        @api.query('Products', :limit => 150)['result'].count.should > 100
+      end
+    end
   end
 end
