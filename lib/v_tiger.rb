bbc60115caaa48f450d3d9c4de032cb8ec7caa1c
Maxim Pechnikov <parallel588@gmail.com>
2015-02-11 09:46:13 +0300

    fix gemspec

--- lib/v_tiger.rb
+++ lib/v_tiger.rb
@@ -1,11 +1,12 @@
 require "v_tiger/version"
 require "v_tiger/api"
-# require "vtiger/query"
+#require "v_tiger/query"
 
 require 'httparty'
 require 'json'
 YAML::ENGINE.yamler = "syck"
 
+require 'active_support'
 require 'active_support/time'
 require 'active_support/core_ext/numeric'
 require 'active_support/core_ext/class/attribute_accessors'
@@ -15,36 +16,36 @@ class VTiger
   cattr_accessor :user, :key, :uri, :logging
   include VTiger::API
   # include VTiger::Query
-  
+
   def self.config(config = {})
     VTiger.user     = config[:user]
     VTiger.key      = config[:key]
     VTiger.uri      = config[:uri]
     VTiger.logging  = config[:logging]
   end
-  
+
   def get(operation, query = {})
     query = {:operation => operation, :sessionName => VTiger.session}.merge(query)
     response = HTTParty.get(VTiger.uri, :query => query)
     log(response) if VTiger.logging
     response.parsed_response
   end
-  
+
   def post(operation, body = {})
     body = {:operation => operation, :sessionName => VTiger.session}.merge(body)
     response = HTTParty.post(VTiger.uri, :body => body)
     log(response) if VTiger.logging
     response.parsed_response
   end
-  
+
   def log(response)
     puts "URI: #{response.request.uri.to_s}"
     puts "BODY: #{response.body}"
     puts ''
   end
-  
+
   private
-  
+
     def self.session
       unless @session && @challenge['expireTime'] > 30.seconds.from_now.to_i
         @challenge  = HTTParty.get(uri, :query => {:operation => 'getchallenge', :username => user}).parsed_response['result']
--- v_tiger.gemspec
+++ v_tiger.gemspec
@@ -17,10 +17,11 @@ Gem::Specification.new do |s|
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
-  
+
   s.add_dependency('httparty')
   s.add_dependency('activesupport')
   s.add_dependency('i18n')
-  
+  s.add_dependency('syck', '~> 1.0.5')
+
   s.add_development_dependency('rspec')
 end
